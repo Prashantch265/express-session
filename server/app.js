05f3030b79ccbd9a0fd1e0506911796d736dc70d
Prashant Chaudhary <Prashantchy265@gmail.com>
2021-05-23 14:50:39 +0545

    basic session setup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,34 @@
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('../database/models/index');
 
 const app = express();
+
+const sessionStore = new SequelizeStore({db: sequelize});
+
+app.use(session({
+    secret: "YOUR_SECRET",
+    store: sessionStore,
+    saveUninitialized: false,
+    resave: false,
+    ssl: true,
+    cookie:{
+        maxAge: 1000 * 60 //1 min
+    }
+}));
+
+app.get('/', (req, res) => {
+    // console.log(req.session);
+
+    if(req.session.viewCount){
+        req.session.viewCount++;
+    }else{
+        req.session.viewCount = 1;
+    }
+
+    res.send(`Page is visited for ${req.session.viewCount} time`);
+});
+
 
 module.exports = app;

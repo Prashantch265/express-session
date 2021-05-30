cf5d3032e514e56f955639c17b2d754333b022d3
Prashant Chaudhary <Prashantchy265@gmail.com>
2021-05-30 17:20:15 +0545

    removed session store

--- server/app.js
+++ server/app.js
@@ -1,40 +1,20 @@
-const express = require('express');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sequelize = require('../database/models/index');
+const express = require("express");
+const session = require("express-session");
 
 const app = express();
 
-const sessionStore = new SequelizeStore({db: sequelize});
+app.use(session({ secret: "YOUR_SECRET" }));
 
-app.use(session({
-    secret: "YOUR_SECRET",
-    store: sessionStore,
-    saveUninitialized: false,
-    resave: false,
-    ssl: true,
-    cookie:{
-        maxAge: 1000 * 60 //1 min
-    }
-}));
+app.get("/", (req, res) => {
+  // console.log(req.session);
 
-try {
-    sessionStore.sync({ force: true }).then(() => console.log("DB connected"));
-  } catch (err) {
-    console.log(err);
+  if (req.session.viewCount) {
+    req.session.viewCount++;
+  } else {
+    req.session.viewCount = 1;
   }
 
-app.get('/', (req, res) => {
-    // console.log(req.session);
-
-    if(req.session.viewCount){
-        req.session.viewCount++;
-    }else{
-        req.session.viewCount = 1;
-    }
-
-    res.send(`Page is visited for ${req.session.viewCount} time`);
+  res.send(`Page is visited for ${req.session.viewCount} time`);
 });
-
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -2,3 +2,10 @@ const sequelize = require("../database/models");
 const app = require("./app");
 
 app.listen(3000, () => console.log("server started on 3000"));
+
+sequelize
+  .sync({ force: true })
+  .then(() => "DB connected")
+  .catch((err) => {
+    console.log(err);
+  });
